d92abfd63eed5d24ede863124f7f7e35ef0aace5
NikhilSinghRathod <r7445580@gmail.com>
2025-01-07 20:36:45 +0530

    my first upload

---
+++ Addition.java
@@ -0,0 +1,3 @@
+import java.util.*;
+class Addition{
+ public static void main(String[] args){
---
+++ Printnumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Printnumbers {
+    public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+     System.out.print("Enter the number");
+     int n =sc.nextInt();
+    for(int i=n;i>=1;i--){
+     System.out.print(i+" ");
+    }
+    System.out.println(" ");
+  }
+}
